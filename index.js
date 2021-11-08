8653699b3d363e6f112de967cb008830a7ec8144
vladimir <vladimirsaakian@gmail.com>
2021-11-08 10:07:41 -0800

    completed task 2

--- index.js
+++ index.js
@@ -71,7 +71,9 @@ NOTE: This will be a callback function for the tasks below
 
 function inning(/*Code Here*/){
     /*Code Here*/
+    return Math.floor(Math.random() * 2);
 }
+console.log(inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
