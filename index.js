51b769693689a81e58441ad4e507543f1eaa5ffe
vladimir <vladimirsaakian@gmail.com>
2021-11-08 09:59:20 -0800

    completed task 1

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  counter1 the var is inside the function while counter2 is outside the function 
   
   2. Which of the two uses a closure? How can you tell?
+
+  counter2 because let count = 0 is outside of the function
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+     counter1 is better practice 
+
 */
 
 // counter1 code
